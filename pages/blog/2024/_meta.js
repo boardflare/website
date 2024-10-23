110b585cb921e29cd16929e8d55cd7453b4a02af
Brent Bolleman <bbolleman@gmail.com>
2024-10-22 21:26:41 -0700

    add blog post on aligning

--- pages/blog/2024/_meta.js
+++ pages/blog/2024/_meta.js
@@ -1,4 +1,5 @@
 export default {
+  "align-with-python-excel": "Functions and aligning with Python in Excel",
   "python-in-excel": "Python in Excel",
   "translate-excel": "Excel TRANSLATE Function",
   "googletranslate-excel": "GOOGLETRANSLATE",
